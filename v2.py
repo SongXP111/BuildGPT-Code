d2dbb7d6342f18444dd67bce2efbc689222e426c
xipeng <sxp0728@126.com>
2026-05-16 19:58:55 -0700

    feat: replace single attention head with multi-head attention in model architecture

--- v2.py
+++ v2.py
@@ -104,7 +104,7 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.sa_head = MultiHeadAttention(4, n_embd // 4) # 4 heads, each with head_size=n_embd/4
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
