9a24c510508a0152cd9b054615863f5187b6e93f
xipeng <sxp0728@126.com>
2026-05-17 19:43:13 -0700

    Update Residual connection

--- v2.py
+++ v2.py
@@ -108,6 +108,7 @@ class FeedForward(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(n_embd, n_embd),
             nn.ReLU(),
+            nn.Linear(n_embd, n_embd),
         )
 
     def forward(self, x):
